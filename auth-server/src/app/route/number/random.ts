72e447a44cb4aeb487845cd80746a7e0454ba3c0
ZakariaTalhami <zakaria.a.a.talhami@gmail.com>
2022-10-05 22:57:14 +0300

    fix: auth-server | random number response format

--- auth-server/src/app/route/number/random.ts
+++ auth-server/src/app/route/number/random.ts
@@ -1,14 +1,16 @@
-import { Request, Response, Router } from 'express';
-import { httpResponse } from '../../utils/httpResponse';
+import { Request, Response, Router } from "express";
+import { httpResponse } from "../../utils/httpResponse";
 
 const router = Router();
 
 router.get("/", async (req: Request, res: Response) => {
-    httpResponse.ok(res, {
-        data: {
-            random: Math.floor(Math.random() * 100)
-        }
-    }, "")
+  httpResponse.ok(
+    res,
+    {
+      random: Math.floor(Math.random() * 100),
+    },
+    ""
+  );
 });
 
 export { router as randomNumberRouter };
